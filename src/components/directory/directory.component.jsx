29efc5a36bd3c28d9e468506d7270e639ffd73c8
Martin Abreu <martin.abreu@live.com>
2020-04-14 14:54:05 -0700

    Added React Router and withRouterHOC to Menu-Items

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,36 +10,36 @@ class DirectoryMenu extends React.Component {
       sections: [
         {
           title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          imgUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
-          linkUrl: "shop/hats",
+          linkUrl: "hats",
         },
         {
           title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          imgUrl: "https://i.ibb.co/px2tCc3/jackets.png",
           id: 2,
-          linkUrl: "shop/jackets",
+          linkUrl: "",
         },
         {
           title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          imgUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
           id: 3,
-          linkUrl: "shop/sneakers",
+          linkUrl: "",
         },
         {
           title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          imgUrl: "https://i.ibb.co/GCCdy8t/womens.png",
           size: "large",
           id: 4,
-          linkUrl: "shop/womens",
+          linkUrl: "",
           size: "large",
         },
         {
           title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          imgUrl: "https://i.ibb.co/R70vBrQ/men.png",
           size: "large",
           id: 5,
-          linkUrl: "shop/mens",
+          linkUrl: "",
           size: "large",
         },
       ],
@@ -49,8 +49,8 @@ class DirectoryMenu extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, id, imageUrl, size }) => (
-          <MenuItem key={id} title={title} imgUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSections }) => (
+          <MenuItem key={id} {...otherSections} />
         ))}
       </div>
     );
---
+++ src/pages/hatspage/hatspage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./hatspage.styles.scss";
+import DirectoryMenu from "./../../components/directory/directory.component";
+
+const HatsPage = () => (
+  <div className="hatspage">
+    <h1>Hats</h1>
+  </div>
+);
+
+export default HatsPage;
