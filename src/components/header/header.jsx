e9579a79430e6581f5799c464472b2842841825d
Martin Abreu <martin.abreu@live.com>
2020-04-21 14:13:26 -0700

    Added logo and styling

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 
 import "./header.scss";
 
 const Header = () => (
   <div className="header">
-    <div className="header-left">
-      <Link to="/">Home</Link>
-      <Link to="/shop">Shop</Link>
+    <Link to="/" className="header-left">
+      <Logo className="logo" />
+    </Link>
+    <div className="header-right">
+      <Link className="links" to="/shop">
+        Shop
+      </Link>
+      <Link className="links" to="/contact">
+        Contact
+      </Link>
     </div>
-    <div className="header-right"></div>
   </div>
 );
 
